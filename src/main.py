1f4f9613afa85fa0741dcb6fed9f081e7ad7e0b2
yun-si <110306079@g.nccu.edu.tw>
2024-03-31 20:45:45 +0800

    Add db testing api

--- src/main.py
+++ src/main.py
@@ -5,7 +5,12 @@ from concurrent.futures import ProcessPoolExecutor
 
 from fastapi import FastAPI
 
+# 引入 pg api router
+from .db.pg_api import router as pg_router
+
 app = FastAPI()
+
+app.include_router(pg_router)
 
 
 @app.get("/")
