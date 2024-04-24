a0b5ba44aa5701724a52eb1aede42e9d6eededbd
Esther125 <friday50523@gmail.com>
2024-04-24 22:28:55 +0800

    collect group chat record

--- src/chatbot/main.py
+++ src/chatbot/main.py
@@ -1,4 +1,7 @@
+import json
 import os
+from datetime import datetime
+from typing import Dict, List
 
 import uvicorn
 from dotenv import load_dotenv
@@ -8,32 +11,79 @@ from linebot import LineBotApi
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from linebot.v3.webhook import WebhookHandler
+from models import MsgDetail
 
 app = FastAPI()
 
-# get environment variables
+summary_queue: Dict[str, List[MsgDetail]] = {}
+
+# Get environment variables
 load_dotenv()
 line_bot_api = LineBotApi(os.getenv("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 if line_bot_api is None or handler is None:
-    print("Make sure you have the right environment variables.")
+    print("Make sure you fill in the right environment variables.")
+    exit(1)
 
 
+# Deal with messages
 @app.post("/callback")
 async def line_webhook(request: Request):
-    body = await request.body()
-    signature = request.headers["X-Line-Signature"]
+    body_bytes = await request.body()
+    body = json.loads(body_bytes.decode("utf-8"))
+    events = body["events"]
+    signature = request.headers.get("X-Line-Signature")
 
     try:
-        handler.handle(body.decode(), signature)
+        handler.handle(body_bytes.decode("utf-8"), signature)
     except InvalidSignatureError:
         return JSONResponse(  # noqa
             status_code=400, content={"message": "Invalid signature"}  # noqa
         )  # noqa
+
+    for event in events:
+        if (
+            event["source"]["type"] == "group"
+            and event["message"]["type"] == "text"  # noqa
+        ):
+            await group_chat(event)
+            print("SUMMARY QUEUE:", summary_queue)
+        elif (
+            event["source"]["type"] == "user"
+            and event["message"]["type"] == "text"  # noqa
+        ):  # noqa
+            await user_chat(event)
+        else:
+            print("The message is not in form of text.")
+
     return JSONResponse(status_code=200, content={"message": "OK"})
 
 
-# webhook endpoint for dialogflow
+async def group_chat(event):
+    message = event["message"]
+
+    if "groupId" in event["source"]:
+        group_id = event["source"]["groupId"]
+        user_id = event["source"]["userId"]
+
+        profile = line_bot_api.get_group_member_profile(group_id, user_id)
+        user_name = profile.display_name
+
+        # Append the message to the group's queue
+        msg_detail = MsgDetail(
+            MsgText=message["text"], UserName=user_name, Time=datetime.now()
+        )
+
+        if group_id not in summary_queue:
+            summary_queue[group_id] = []
+        summary_queue[group_id].append(msg_detail)
+
+
+async def user_chat(events):
+    pass
+
+
+# Webhook endpoint for dialogflow
 @app.post("/webhook")
 async def webhook(request: Request):
     req_data = await request.json()
