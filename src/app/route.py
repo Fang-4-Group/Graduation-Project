ac7bd499130343c306b0a5266ee8aeee7afca805
Esther125 <friday50523@gmail.com>
2024-05-05 00:03:59 +0800

    solve conflict

--- src/app/route.py
+++ src/app/route.py
@@ -1,42 +1,73 @@
 from fastapi import APIRouter
+from fastapi.responses import FileResponse
 
-from database.seeds import mongo_api_for_testing as mg_api_t
-from database.seeds import pg_api_for_testing as pg_api_t
+from database.seeds.mongo_api_for_testing import MongoDBClient
+from database.seeds.pg_api_for_testing import DatabaseClient
 
 router = APIRouter()
 
 
 @router.get("/test_create/")
 async def test_create():
-    result = await pg_api_t.test_create_fun()
+    client = DatabaseClient()
+    result = await client.test_create_fun()
     return result
 
 
 @router.get("/test_select/")
 async def test_select():
-    result = await pg_api_t.test_select_fun()
+    client = DatabaseClient()
+    result = await client.test_select_fun()
     return result
 
 
 @router.get("/test_drop/")
 async def test_drop():
-    result = await pg_api_t.test_drop_fun()
+    client = DatabaseClient()
+    result = await client.test_drop_fun()
     return result
+
+
+@router.get("/test_create_img/")
+async def test_create_img():
+    client = DatabaseClient()
+    result = await client.test_create_img_fun()
+    return result
+
+
+@router.get("/show_img/")
+async def show_img():
+    client = DatabaseClient()
+    data = await client.test_select_img_fun()
+    return FileResponse(data["message"][0]["path"])
+
+
+@router.get("/test_drop_img/")
+async def test_drop_img():
+    client = DatabaseClient()
+    result = await client.test_drop_img_fun()
+    return result
+
+
+# MongoDB
 
 
 @router.get("/test_insert_mongo/")
 async def test_insert_mg():
-    result = await mg_api_t.test_insert_fun()
+    client = MongoDBClient()
+    result = await client.test_insert_fun()
     return result
 
 
 @router.get("/test_select_mongo/")
 async def test_select_mg():
-    result = await mg_api_t.test_select_fun()
+    client = MongoDBClient()
+    result = await client.test_select_fun()
     return result
 
 
 @router.get("/test_drop_mongo/")
 async def test_drop_mg():
-    result = await mg_api_t.test_drop_fun()
+    client = MongoDBClient()
+    result = await client.test_drop_fun()
     return result
