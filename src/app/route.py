c5d019c701641f84445402c6bdc47db80f8c54e7
Esther125 <friday50523@gmail.com>
2024-05-05 22:09:48 +0800

    solve conflict

--- src/app/route.py
+++ src/app/route.py
@@ -1,43 +1,28 @@
 from fastapi import APIRouter
-<<<<<<< HEAD
 from fastapi.responses import FileResponse
 
 from database.seeds.mongo_api_for_testing import MongoDBClient
 from database.seeds.pg_api_for_testing import DatabaseClient
-=======
-
-from database.seeds import mongo_api_for_testing as mg_api_t
-from database.seeds import pg_api_for_testing as pg_api_t
->>>>>>> 251e433 (save group chat records to MongoDB)
 
 router = APIRouter()
 
 
 @router.get("/test_create/")
 async def test_create():
-<<<<<<< HEAD
     client = DatabaseClient()
     result = await client.test_create_fun()
-=======
-    result = await pg_api_t.test_create_fun()
->>>>>>> 251e433 (save group chat records to MongoDB)
     return result
 
 
 @router.get("/test_select/")
 async def test_select():
-<<<<<<< HEAD
     client = DatabaseClient()
     result = await client.test_select_fun()
-=======
-    result = await pg_api_t.test_select_fun()
->>>>>>> 251e433 (save group chat records to MongoDB)
     return result
 
 
 @router.get("/test_drop/")
 async def test_drop():
-<<<<<<< HEAD
     client = DatabaseClient()
     result = await client.test_drop_fun()
     return result
@@ -71,38 +56,18 @@ async def test_drop_img():
 async def test_insert_mg():
     client = MongoDBClient()
     result = await client.test_insert_fun()
-=======
-    result = await pg_api_t.test_drop_fun()
-    return result
-
-
-# MongoDB
-
-
-@router.get("/test_insert_mongo/")
-async def test_insert_mg():
-    result = await mg_api_t.test_insert_fun()
->>>>>>> 251e433 (save group chat records to MongoDB)
     return result
 
 
 @router.get("/test_select_mongo/")
 async def test_select_mg():
-<<<<<<< HEAD
     client = MongoDBClient()
     result = await client.test_select_fun()
-=======
-    result = await mg_api_t.test_select_fun()
->>>>>>> 251e433 (save group chat records to MongoDB)
     return result
 
 
 @router.get("/test_drop_mongo/")
 async def test_drop_mg():
-<<<<<<< HEAD
     client = MongoDBClient()
     result = await client.test_drop_fun()
-=======
-    result = await mg_api_t.test_drop_fun()
->>>>>>> 251e433 (save group chat records to MongoDB)
     return result
