727b30462b57936097fb9f80031561def62f3647
Jeaninetang <94062751+Jeaninetang@users.noreply.github.com>
2024-07-23 21:28:44 +0800

    [GP-116]Channge Homeold & Homeyoung to words instead of numbers

--- frontend/src/components/HomeOld.vue
+++ frontend/src/components/HomeOld.vue
@@ -110,10 +110,10 @@
   const name = ref("")
   const useremail = ref("jony12@gmail.com")
 
-  const drink = ref(0);
-  const smoke = ref(0);
-  const clean_habit = ref(0);
-  const sleepTime = ref(0);
+  const drink = ref("");
+  const smoke = ref("");
+  const clean_habit = ref("");
+  const sleepTime = ref("");
   const characters = ref([]);
   const interests = ref([]);
   const mbti = ref();
@@ -137,16 +137,65 @@
       console.log("Name data fetched:", name_Response.data);
 
       const sleep_Response = await axios.get(`${BASE_URL}/get_sleep_time/${People_ID}`); 
-      sleepTime.value = sleep_Response.data.sleep_time;
+      const s= sleep_Response.data.sleep_time;
+      if(s == 1){
+        sleepTime.value = "晚上7:00~9:00";
+      }else if(s == 2){
+        sleepTime.value = "晚上9:00~11:00";
+      }else if(s == 3){
+        sleepTime.value = "晚上11:00~隔天1:00";
+      }else if(s == 4){
+        sleepTime.value ="凌晨1:00~3:00";
+      }else if(s == 5){
+        sleepTime.value = "凌晨3:00以後";
+      }
 
       const drink_Response = await axios.get(`${BASE_URL}/get_drink/${People_ID}`); 
-      drink.value = drink_Response.data.drink;
+      const d = drink_Response.data.drink;
+      if(d == 0){
+        drink.value = "從不飲酒";
+      }else if(d == 1){
+        drink.value = "很少飲酒";
+      }else if(d == 2){
+        drink.value = "偶爾小酌";
+      }else if(d == 3){
+        drink.value = "經常飲酒";
+      }else if(d == 4){
+        drink.value ="每天飲酒";
+      }else if(d == 5){
+        drink.value = "酗酒";
+      }
+      
 
       const smoke_Response = await axios.get(`${BASE_URL}/get_smoke/${People_ID}`); 
-      smoke.value = smoke_Response.data.smoke;
+      const sm = smoke_Response.data.smoke;
+      if(sm == 0){
+        smoke.value = "從不抽菸";
+      }else if(sm == 1){
+        smoke.value = "很少抽菸";
+      }else if(sm == 2){
+        smoke.value = "偶爾抽菸";
+      }else if(sm == 3){
+        smoke.value = "經常抽菸";
+      }else if(sm == 4){
+        smoke.value ="每天抽菸";
+      }else if(sm == 5){
+        smoke.value = "重度菸癮";
+      }
 
       const clean_habit_Response = await axios.get(`${BASE_URL}/get_clean_habit/${People_ID}`); 
-      clean_habit.value = clean_habit_Response.data.clean_habit;
+      const c = clean_habit_Response.data.clean_habit;
+      if(c == 1){
+        clean_habit.value = "不愛乾淨";
+      }else if(c == 2){
+        clean_habit.value = "稍微愛乾淨";
+      }else if(c == 3){
+        clean_habit.value = "正常愛乾淨";
+      }else if(c == 4){
+        clean_habit.value ="很愛乾淨";
+      }else if(c == 5){
+        clean_habit.value = "極度潔癖";
+      }
 
       const characters_Response = await axios.get(`${BASE_URL}/get_characters/${People_ID}`); 
       characters.value = characters_Response.data.characters;
