ed68fa2c158102a7c832819bae0bbd70bb427d0f
frafishen <104518723+frafishen@users.noreply.github.com>
2024-08-12 21:46:43 +0800

    fix

--- src/main.py
+++ src/main.py
@@ -5,11 +5,8 @@ import time
 from concurrent.futures import ProcessPoolExecutor
 
 from fastapi import FastAPI
-from starlette.middleware.sessions import SessionMiddleware
-
 from fastapi.middleware.cors import CORSMiddleware
-
-
+from starlette.middleware.sessions import SessionMiddleware
 
 # 引入 pg api router
 from .app.route import router
@@ -26,7 +23,7 @@ origins = [
     "http://localhost",
     "http://localhost:7877",
     "http://localhost:8081",
-    "http://localhost:5432"
+    "http://localhost:5432",
     # 添加您允许的其他域
 ]
 
@@ -36,7 +33,6 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
-    
 )
 
 
