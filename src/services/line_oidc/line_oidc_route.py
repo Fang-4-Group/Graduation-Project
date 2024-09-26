48a9e10f5284ac27ae1b20fadc4103bcb5521b0a
Esther125 <friday50523@gmail.com>
2024-09-26 22:05:38 +0800

    feat: add callback router

--- src/services/line_oidc/line_oidc_route.py
+++ src/services/line_oidc/line_oidc_route.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
 
 from services.line_oidc.line_oidc_service import LineOIDCService
@@ -15,3 +15,12 @@ service = LineOIDCService()
 async def line_login():
     auth_url = service.generate_auth_url()
     return RedirectResponse(auth_url)
+
+
+@router.get("/callback")
+async def handle_callback(request: Request):
+    code = request.query_params.get("code")
+    token_response = await service.exchange_code_for_token(code)
+    access_token = token_response.get("access_token")
+    userinfo = await service.get_user_info(access_token)
+    return userinfo
